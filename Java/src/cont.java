15500859bf92d78a43f3f55ca30b4146d3d8cc44
Kevin <kevindasilvalima2008@gmail.com>
2026-04-07 10:04:17 -0300

    Add files via upload

---
+++ Java/src/cont.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class cont {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 30; i >= 1; i--) {
+            System.out.println("ESTE E O NUMERO: " + i + " DO  FOR");
+        }
+
+        System.out.println("");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("ESTE E O NUMERO: " + i + " DO  FOR");
+        }
+    }
+}
